92a7169c0195a9d9a4eb39d1ebdc63096953a933
MSI\babbi <babbigiovanni@gmail.com>
2026-04-20 10:57:47 +0200

    Added workflow

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+    application
+}
+
+repositories { // Where to search for dependencies
+    mavenCentral()
+}
+
+dependencies {
+    // Suppressions for SpotBugs
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.8")
+
+    // Maven dependencies are composed by a group name, a name and a version, separated by colons
+    implementation("com.omertron:API-OMDB:1.5")
+    implementation("org.jooq:jool:0.9.15")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.32")
+}
+
+application {
+    mainClass.set("Main")
+}
