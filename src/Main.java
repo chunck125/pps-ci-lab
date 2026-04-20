1d94c0951a2f50a589945523b5fd6d5a36665cce
MSI\babbi <babbigiovanni@gmail.com>
2026-04-20 11:26:18 +0200

    added doc on the main class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+/**
+ * This is a sample Java application.
+ * You can run it using the command: `./gradlew run`
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
